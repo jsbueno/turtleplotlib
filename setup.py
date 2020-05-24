497393a0b8e578b27d7066b4150cece3f4693ab3
Joao S O Bueno <gwidion@gmail.com>
2020-05-24 20:11:08 -0300

    Initial setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="turtleplotlib",
+    version="0.1.0",
+    description="Graphic Turtle to be used within a Matplotlib Figure",
+    url="http://github.com/jsbueno/turtleplotlib",
+    author="João S. O. Bueno",
+    author_email="gwidion@gmail.com",
+    license="LGPLv3",
+    packages=["turtleplotlib"],
+    zip_safe=True,
+)
